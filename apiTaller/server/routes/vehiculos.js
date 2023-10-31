15eab7e3c846d46b720db2928f715ed844b32cf3
lgarces <luis.garces.sepulveda@ciisa.cl>
2023-10-31 13:03:31 -0300

    Cambio en ruta - para busqueda por patente 31-10-2023 13.03

--- apiTaller/server/routes/vehiculos.js
+++ apiTaller/server/routes/vehiculos.js
@@ -5,8 +5,8 @@ module.exports=(app)=>{
     app.post('/api/vehiculos',mdAuth.auth,vehiculosController.crearVehiculo);
     app.put('/api/vehiculos/:id',mdAuth.auth,vehiculosController.editarVehiculo);
     app.get('/api/vehiculos/:id',mdAuth.auth,vehiculosController.buscarVehiculo)
-    app.get('/api/vehiculos/patente/:nroPatente',mdAuth.auth,vehiculosController.buscarVehiculoxPatente)
     app.get('/api/vehiculos/',mdAuth.auth,vehiculosController.listarVehiculos)
     app.get('/api/vehiculos/general',mdAuth.auth,vehiculosController.listarVehiculosGeneral)
+    app.get('/api/vehiculos/general/:nroPatente',mdAuth.auth,vehiculosController.buscarVehiculoxPatente)
     app.put('/api/vehiculos/:id',mdAuth.auth,vehiculosController.eliminarVehiculo)
 }
